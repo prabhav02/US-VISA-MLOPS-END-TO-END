85da0984142391b61d6516947ae89c5865b05e59
prabhav02 <febindian2004@gmail.com>
2025-02-17 18:31:55 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Prabhav",
+    author_email="febindian2004@gmail.com",
+    packages=find_packages()
+)
