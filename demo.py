ed083e8465b155f4fee1b4351df579931de9e19d
prabhav02 <febindian2004@gmail.com>
2025-02-18 10:26:20 +0530

    Changes done

--- demo.py
+++ demo.py
@@ -1,10 +1,6 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-#logging.info("Welcome to our custom log")
-try:
-    a=2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj=TrainPipeline()
+obj.run_pipeline()
 
 
