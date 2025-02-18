e7497dfd276599e5bbbd7b6ea53e9ec1e6a50977
prabhav02 <febindian2004@gmail.com>
2025-02-18 19:56:46 +0530

    model pusher done

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -27,7 +27,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-REGION_NAME = "us-east-1"
+REGION_NAME = "eu-north-1"
 
 
 
@@ -73,7 +73,7 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 MODEL EVALUATION related constant 
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_BUCKET_NAME = "usvisamodel20251"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
